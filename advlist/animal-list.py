e05ee385cad65a1175cec6fcbf239f78614cc340
Justin Suen <justinmsuen@users.noreply.github.com>
2022-03-02 22:21:41 +0000

    about complex lists

---
+++ advlist/animal-list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Alta3 Research | RZFeeser
+   List - making selections from complex lists"""
+
+def main():
+
+    animalList = ["Fox", "Fly", "Ant", "Bee", "Cod", "Cat", "Dog"]
+
+    print(animalList)
+
+main()
