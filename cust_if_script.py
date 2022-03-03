2480fb5ee8c507274deaee4e540975952c5633e4
Justin Suen <justinmsuen@users.noreply.github.com>
2022-03-03 21:36:26 +0000

    writing custom code (if-elif-else)

---
+++ cust_if_script.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+def main():
+
+
+    grade = input("Enter your numeric grade(type exit to stop): ")
+    if(grade.lower() == "exit"):
+        print("Exiting script")
+        exit()
+
+    grade = int(grade)
+    print("You entered:", grade)
+
+    if (grade >= 90 and grade <= 100):
+        print("You got an A")
+    elif (grade >= 80 and grade < 90):
+        print("You got a B")
+    elif(grade >= 70 and grade < 80):
+        print("You got a C")
+    elif(grade >= 60 and grade < 70):
+        print("You got a D")
+    elif(grade > 100):
+        print("Not possible, nice try")
+    else:
+        print("You Failed")
+
+if __name__ == "__main__":
+    main()
